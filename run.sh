f42a1c80f7fc689638fbf82de26afbfd7d93b4ee
KuizW9 <kuiz2sw@gmail.com>
2025-06-14 04:50:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #python3 ./djson.py
 
-[ -f ~/.bashrc ] || touch ~/.bashrc
-sed -i '/run.sh/d' ~/.bashrc
-echo "bash ~/us-socks/run.sh" >> ~/.bashrc
-source ~/.bashrc
+#[ -f ~/.bashrc ] || touch ~/.bashrc
+#sed -i '/run.sh/d' ~/.bashrc
+#echo "bash ~/us-socks/run.sh" >> ~/.bashrc
+#source ~/.bashrc
 
 # Kill process from sbpid
 [ -f ./sbpid.log ] && kill $(cat ./sbpid.log) && echo "Killed process from sbpid"
@@ -11,4 +11,4 @@ source ~/.bashrc
 [ -f ./cfpid.log ] && kill $(cat ./cfpid.log) && echo "Killed process from cfpid"
 nohup ./cloudflared tunnel --no-autoupdate --edge-ip-version auto --protocol http2 run --token eyJhIjoiNjQ1MTEzYmM3MWQ0MDgwMzA2ZmFmMWJhMmYyZmM4MGEiLCJ0IjoiYzA2Y2EyN2MtZjFjYy00YzFkLWE5N2ItM2Q2ZDNkYTc1NWI0IiwicyI6Ik5qTTVaamhrTlRVdE0yTXlNaTAwWTJZMkxXRmxNbUl0WWprME1qWTVNVEUyTnprMCJ9  >/dev/null 2>&1 & echo "$!" > ./cfpid.log
 nohup ./sing-box run  >/dev/null 2>&1 & echo "$!" > ./sbpid.log
-bash ./docker.sh
+bash ./docker2.sh
